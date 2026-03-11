ecde267f19d3533ab2ecbd80ee49c696906c0d26
Chaosvanguard <rileystone34@gmail.com>
2026-03-10 21:49:44 -0400

    Wrong version

--- MakersPath.lua
+++ MakersPath.lua
@@ -2,7 +2,7 @@ local ADDON_NAME, MakersPath = ...
 
 MakersPath = MakersPath or {}
 MakersPath.name = ADDON_NAME
-MakersPath.version = "1.4.2"
+MakersPath.version = "1.4.3"
 _G.MakersPath = MakersPath
 local debugprofilestop = debugprofilestop
 MakersPath.Config = MakersPath.Config or {}
