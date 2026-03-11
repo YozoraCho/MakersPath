30ff378fedd288df0cd933276ce8f8abf153f791
Chaosvanguard <rileystone34@gmail.com>
2026-03-10 21:48:13 -0400

    Forgot to update version

--- MakersPath.lua
+++ MakersPath.lua
@@ -2,7 +2,7 @@ local ADDON_NAME, MakersPath = ...
 
 MakersPath = MakersPath or {}
 MakersPath.name = ADDON_NAME
-MakersPath.version = "1.4.1"
+MakersPath.version = "1.4.2"
 _G.MakersPath = MakersPath
 local debugprofilestop = debugprofilestop
 MakersPath.Config = MakersPath.Config or {}
