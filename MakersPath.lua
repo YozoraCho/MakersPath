92d007f4ade84cc5d846099dabd9abb2ec89fcc2
Chaosvanguard <rileystone34@gmail.com>
2026-03-16 11:46:51 -0400

    V

--- MakersPath.lua
+++ MakersPath.lua
@@ -2,7 +2,7 @@ local ADDON_NAME, MakersPath = ...
 
 MakersPath = MakersPath or {}
 MakersPath.name = ADDON_NAME
-MakersPath.version = "1.4.7"
+MakersPath.version = "1.4.8"
 _G.MakersPath = MakersPath
 local debugprofilestop = debugprofilestop
 MakersPath.Config = MakersPath.Config or {}
